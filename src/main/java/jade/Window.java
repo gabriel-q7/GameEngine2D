124929e48633828703ed2dcc96715a398101f732
gabriel <gabriel_aq7@hotmail.com>
2022-02-15 09:51:33 -0300

    upd Window

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -1,8 +1,18 @@
 package jade;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+
 public class Window {
     private int width, height;
     private String title;
+    private long glfwWindow;
+
     private static Window window = null;
 
     private Window() {
@@ -19,6 +29,48 @@ public class Window {
     }
 
     public void run() {
+        System.out.println("Hello LWJGL" + Version.getVersion());
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        //Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //Initialize GLFW
+        if(!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        //Confire GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        //Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        //Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        //Enable v-sync
+        glfwSwapInterval(1);
+
+        //Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        //This line is critical for LWJGL's interoperation with GLFW's
+        //OpenGL context, or any context that is managed externally.
+        GL.createCapabilities();
+    }
+
+    public void loop() {
 
     }
 }
